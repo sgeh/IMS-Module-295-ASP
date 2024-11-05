d6a34569e0fceec1cae1ec2abd0748d845d27666
Silvan Gehrig <silvan.gehrig@ost.ch>
2024-11-05 13:30:27 +0100

    Add tips for repetition exercise.

--- 04_EF_DI/01_Repetition_0/01_Vorlage/Controllers/CounterController.cs
+++ 04_EF_DI/01_Repetition_0/01_Vorlage/Controllers/CounterController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Bwz.Rappi.CounterApp.Controllers
 {
     /// <summary>
-    /// Data object for the counter.
+    /// Data object for the counter (JSON representation).
     /// </summary>
     public class Counter
     {
@@ -14,12 +14,15 @@ namespace Bwz.Rappi.CounterApp.Controllers
     /// <summary>
     /// Provides an API for managing the Counter.
     /// </summary>
+    // TODO: Add required [Annotation]s
     public class CounterController : ControllerBase
     {
         private static int _current = 0;
 
+        // TODO: Implement Method
         // public IActionResult Post() { }
 
+        // TODO: Add required [Annotation]
         public IActionResult Get()
         {
             var newCounter = new Counter { Current = _current };
